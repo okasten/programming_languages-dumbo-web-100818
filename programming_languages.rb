866e3518cf7320d7a87ae280ebdc8bfbe8cc12bc
Olivia Kasten <odkasten@gmail.com>
2018-09-25 19:17:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  current_style = []
+  languages.each do |style, language|
+    current_style << style
+    
+    language.each do |type|
+      
+      if type[0] == :javascript
+        current_style = [:oo, :functional]
+        #binding.pry
+      elsif type[0] == :python
+        current_style = [:oo]
+      end 
+
+      new_hash[type[0]] = {type[1].keys.join.to_sym => type[1].values.join, :style => current_style}
+    end 
+    current_style = []
+  end 
+  new_hash
 end
